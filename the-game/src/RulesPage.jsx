07ffb51003cbd7e74230adea814161b43c4c3789
juanifdez <juanifdez>
2023-04-19 18:51:05 -0400

    feat(RulesPage): create page for instructions

---
+++ the-game/src/RulesPage.jsx
@@ -0,0 +1,31 @@
+import './RulesPage.css'
+
+export default function RulesPage() {
+    return (
+        <div class="block">
+        <h1>Instrucciones</h1>
+        <h2>Objetivo</h2>
+        <p>Explicación </p>
+        <h2>Categorías</h2>
+        <img src="logos/logo_ciencias.png" width="50" height="50"/>
+        <img src="logos/logo_deportes.png" width="50" height="50"/>
+        <img src="logos/logo_artes.png" width="50" height="50"/>
+        <img src="logos/logo_historia.png" width="50" height="50"/>
+        <img src="logos/logo_matematicas.png" width="50" height="50"/>
+        <img src="logos/logo_cerebro.png" width="50" height="50"/>
+        <p>Explicación </p>
+        <h2>Tablero</h2>
+        <img src="tableros/tablero.png" width="450" height="400"/>
+        <p>Explicación </p>
+        <h2>Fichas</h2>
+        <img src="fichas/ficha_verde.png" width="50" height="50"/>
+        <img src="fichas/ficha_naranja.png" width="50" height="50"/>
+        <img src="fichas/ficha_morada.png" width="50" height="50"/>
+        <img src="fichas/ficha_azul.png" width="50" height="50"/>
+        <img src="fichas/ficha_rosada.png" width="50" height="50"/>
+        <p>Explicación </p>
+        <h2>Reglas especiales</h2>
+        <p>Explicación </p>
+        </div>
+      );
+}
--- the-game/src/main.jsx
+++ the-game/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import LandingPage from './LandingPage'
 import TeamPage from './TeamPage'
+import RulesPage from './RulesPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TeamPage/>
+    <RulesPage/>
   </React.StrictMode>,
 )
