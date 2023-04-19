a8767c0f70af54b635885bd7e91640876c7f9ff9
juanifdez <juanifdez>
2023-04-19 17:40:54 -0400

    feat(LandingPage): create user landing page

---
+++ the-game/src/LandingPage.jsx
@@ -0,0 +1,19 @@
+import './LandingPage.css'
+
+export default function LandingPage() {
+    return (
+        <div class="block">
+        <img src="logo.png" width="100" height="100"/>
+        <h1>Bienvenido a The Game</h1>
+        <p>¡Prepárate para el juego de preguntas trivia más divertido del año!</p>
+        <br />
+        <img src="tablero.png" width="400" height="400"/>
+        <br />
+        <br />
+        <button>Jugar</button>
+        <br />
+        <p>Iniciar sesión <a href="#">Sign in</a></p>
+        <p>Crear cuenta <a href="#">Sign up</a></p>
+        </div>
+      );
+}
