4b9156211b0391fac8331e6287ead307ffd811bc
juanifdez <juanifdez>
2023-04-19 17:50:49 -0400

    feat(TeamPage): create page about team members

---
+++ the-game/src/TeamPage.jsx
@@ -0,0 +1,13 @@
+import './TeamPage.css'
+
+export default function TeamPage() {
+    return (
+        <div class="block">
+        <h1>Nuestro equipo</h1>
+        <img src="equipo.png" width="400" height="400"/>
+        <h2>Diego Valenzuela - Juanita Fernández</h2>
+        <p>Descripcion Diego </p>
+        <p>Descripcion Juanita </p>
+        </div>
+      );
+}
--- the-game/src/main.jsx
+++ the-game/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import LandingPage from './LandingPage'
+import TeamPage from './TeamPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage/>
+    <TeamPage/>
   </React.StrictMode>,
 )
