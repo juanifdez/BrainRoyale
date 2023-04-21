f9a5a23d6444c6f7ce677b92b9ddafdde82c4d73
juanifdez <juanifdez>
2023-04-21 15:21:30 -0400

    feat(RollDice): create dynamic component

--- the-game/src/LandingPage.jsx
+++ the-game/src/LandingPage.jsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
     return (
         <div class="block">
         <img src="logo.png" width="100" height="100"/>
-        <h1>Bienvenido a The Game</h1>
+        <h1>Bienvenido a Brain Royale</h1>
         <p>¡Prepárate para el juego de preguntas trivia más divertido del año!</p>
         <br />
         <img src="tableros/tablero.png" width="400" height="400"/>
