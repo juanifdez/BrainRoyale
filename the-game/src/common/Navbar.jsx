16599250a49b74dc9cd81c9a01962a401152f5c7
Diego <diegovalarq@uc.cl>
2023-04-21 07:15:30 -0400

    feat(navBar): navbar creation, components routing halfway through

---
+++ the-game/src/common/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+export default function Navbar() {
+    return(
+    <div class="block-nav">
+        <div class="nav">
+        <nav class="nav-general">
+            <ul>
+                <li><a href="/../../index.html">Inicio</a></li>
+                <li><a href="/src/views/about.html">El equipo</a></li>
+            </ul>
+        </nav>
+        </div>
+    </div>
+
+
+        );
+}
