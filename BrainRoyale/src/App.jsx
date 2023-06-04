1fdd81ee1a341756856a86b59b7ddd2d0ce1680f
Diego <diegovalarq@uc.cl>
2023-06-04 12:06:08 -0400

    Game: getGame, getPlayers

--- BrainRoyale/src/App.jsx
+++ BrainRoyale/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
 import Routing from './Routing'
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- BrainRoyale/src/pages/Game.jsx
+++ BrainRoyale/src/pages/Game.jsx
@@ -1,18 +1,30 @@
 import Navbar from '../components/Navbar';
 import './Game.css';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { SERVER_URL } from '../App';
+
+// Inspiración en Cápsula 6 = Conexión con API, de semestre 2022-2
 
 export default function Game() {
 
-    return (
+    const playerId = 0;
+    const matchId = 0;
+    const getGame = async () => {
+      const url = `${SERVER_URL}/game/${matchId}`;
+      await axios.get(url).then((responde) => {
+        console.log(responde.data);
+      });
+    };
 
-        <div className="block">
-        <Navbar/>
-        <h1>Mucha Suerte!</h1>
-        <br />
-        <img src="tableros/tablero.png" width="400" height="400"/>
-        
-        
-        </div>
-      );
-}
+    const getPlayers = async () => {
+      const url = `${SERVER_URL}/game/${matchId}/player/${playerId}`;
+      await axios.get(url).then((responde) => {
+        console.log(responde.data);
+      });
+    };
+
+    // lanzar los dados de manera aleatoria
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+  }
